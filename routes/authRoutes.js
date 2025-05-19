9af4159bd82af84b4fe527bb57a3973a029dc294
vthphc <vthphc.dev@gmail.com>
2025-05-19 13:47:57 +0700

    Add login and user retrieval functionality to authentication controllers and routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,7 @@ const registerService = require("../services/auth/register");
 router.post("/send-email", authControllers.sendEmail);
 router.post("/register", registerService.registerUser);
 router.put("/verify-email/:verificationToken", registerService.updateUserVerification);
+router.post("/login", authControllers.login);
+router.get("/me", authControllers.me);
 
 module.exports = router;
---
+++ services/auth/login.js
@@ -0,0 +1,39 @@
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (email, password) => {
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error("Invalid credentials");
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        console.log(isPasswordValid);
+
+        if (!isPasswordValid) {
+            throw new Error("Invalid credentials");
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "24h",
+        });
+
+        return token;
+    } catch (error) {
+        throw new Error("Internal server error");
+    }
+};
+
+const getMe = async (token) => {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+    return user;
+};
+
+module.exports = { loginUser, getMe };
