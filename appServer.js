6ee34629733aa1b0fe5632e107ec7331d4d9cb47
Mingu <vhnm3004@outlook.com>
2025-05-21 17:31:40 +0700

    Added cors

--- appServer.js
+++ appServer.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("./utils/logger");
 const connectToDatabase = require("./config/dbConfig");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,22 +10,23 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5001;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", require("./routes"));
 
 async function startServer() {
-    try {
-        await connectToDatabase();
+	try {
+		await connectToDatabase();
 
-        app.listen(port, () => {
-            logger.info(`Server is running on port ${port}`);
-        });
-    } catch (error) {
-        logger.error("Error starting server:", error);
-        process.exit(1);
-    }
+		app.listen(port, () => {
+			logger.info(`Server is running on port ${port}`);
+		});
+	} catch (error) {
+		logger.error("Error starting server:", error);
+		process.exit(1);
+	}
 }
 
 module.exports = startServer;
