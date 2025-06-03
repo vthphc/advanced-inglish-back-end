19b22f67003ce83a517e7147884366f1354414a3
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-06-03 16:54:54 +0700

    Enhance getTestById function to populate questionsList within lessonList for improved data retrieval.

--- services/rest/testServices.js
+++ services/rest/testServices.js
@@ -11,7 +11,12 @@ const getTestById = async (testId) => {
     }
 
     const test = await Tests.findById(testId)
-        .populate("lessonList")
+        .populate({
+            path: "lessonList",
+            populate: {
+                path: "questionsList",
+            },
+        })
         .populate("comments");
 
     if (!test) {
